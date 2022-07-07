9f57559dc8205bc853b614400a3bc73077517f69
pyo-sh <pygosky@gmail.com>
2022-07-08 06:37:07 +0900

    :sparkles: 추가정보 다른 정보를 위한 레이아웃 작성 - Issue#10

---
+++ public/javascripts/pages/signup/others.js
@@ -0,0 +1,6 @@
+(function initOthers() {
+  const $emailInput = document.querySelector("#others_email");
+  const $nicknameInput = document.querySelector("#others_nickname");
+  const $passwordInput = document.querySelector("#others_password");
+  const $bithdayInput = document.querySelector("#others_birthday");
+})();
